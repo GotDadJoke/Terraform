e7ece51ae319de0225de212de897d2d983e39c00
Yanilda Peralta <yanilda.peralta@gmail.com>
2020-07-23 18:18:40 -0400

    route53

--- route53.tf
+++ route53.tf
@@ -1,8 +1,9 @@
 resource "aws_route53_record" "api_domain_record" {
   zone_id = var.zone_id //no changes needed, see variables.tf file
   name    = "api.gotdadjoke.com"
-  type    = "string"
+  type    = "A"
   ttl     = "300" //time to live, default is ok
-  records = [""] //list of strings resources to point to
+  records = ["10.0.0.1"]
+
 }
 
