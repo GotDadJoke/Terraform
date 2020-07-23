031491ed965defaea78a3ff1b5db2768fd995a90
Yanilda Peralta <yanilda.peralta@gmail.com>
2020-07-23 19:47:52 -0400

    custom domain

--- route53.tf
+++ route53.tf
@@ -1,9 +1,16 @@
+
+resource "aws_api_gateway_domain_name" "api_domain" {
+  certificate_arn = module.acm_certificate.arn
+  domain_name     = "api.gotdadjoke.com"
+  security_policy = "TLS_1_2"
+}
+
+
 resource "aws_route53_record" "api_domain_record" {
   zone_id = var.zone_id //no changes needed, see variables.tf file
   name    = "api.gotdadjoke.com"
   type    = "A"
   ttl     = "300" //time to live, default is ok
   records = ["10.0.0.1"]
-
 }
 
