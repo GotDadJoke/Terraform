42dd13930d349d95dd83b6f018ae7761434041b2
Yanilda Peralta <yanilda.peralta@gmail.com>
2020-07-23 16:38:49 -0400

    updated route file

--- route53.tf
+++ route53.tf
@@ -1,9 +1,8 @@
-resource "aws_route53_record" "your-name-here" {
+resource "aws_route53_record" "api_domain_record" {
   zone_id = var.zone_id //no changes needed, see variables.tf file
-  name    = "type-what-you-need.gotdadjoke.com"
-  type    = "use type you need"
+  name    = "api.gotdadjoke.com"
+  type    = "string"
   ttl     = "300" //time to live, default is ok
   records = [""] //list of strings resources to point to
 }
 
-//documentation: https://www.terraform.io/docs/backends/config.html
