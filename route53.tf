51ffdfb2169ba6d2f91c682c3b55bbcecec1a0fe
AlyonaTiki <31675522+AlyonaTiki@users.noreply.github.com>
2020-07-23 20:51:19 -0400

    updated subdomain api.gotdadjoke.com

--- route53.tf
+++ route53.tf
@@ -1,16 +1,18 @@
-
 resource "aws_api_gateway_domain_name" "api_domain" {
   certificate_arn = module.acm_certificate.arn
   domain_name     = "api.gotdadjoke.com"
   security_policy = "TLS_1_2"
 }
 
-
+//create/update Route53 record
 resource "aws_route53_record" "api_domain_record" {
-  zone_id = var.zone_id //no changes needed, see variables.tf file
-  name    = "api.gotdadjoke.com"
+  name    = aws_api_gateway_domain_name.api_domain.domain_name
   type    = "A"
-  ttl     = "300" //time to live, default is ok
-  records = ["10.0.0.1"]
-}
+  zone_id = var.zone_id
 
+  alias {
+    evaluate_target_health = true
+    name                   = aws_api_gateway_domain_name.api_domain.cloudfront_domain_name
+    zone_id                = aws_api_gateway_domain_name.api_domain.cloudfront_zone_id
+  }
+}
